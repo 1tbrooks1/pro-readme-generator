d87e09cbd8085ece3a14856a32dab3f3a8a990a3
tbrooks <tbrooks321@yahoo.com>
2022-01-18 12:50:42 -0500

    created functions to produce questions

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
-const inquirer = require("inquirer");
-
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const generateMarkdown = require("./util/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is a brief description of your project?",
+        name: "description"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like for your project?",
+        choices: ["MIT", "Academic", "Creative Commons"] 
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+console.log(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log("hello!");
+    inquirer.prompt(
+        questions
+    )
+    .then((answers) => {
+        console.log("Generating README now!");
+        writeToFile("README.md", generateMarkdown(answers));
+    })
 }
 
 // Function call to initialize app
